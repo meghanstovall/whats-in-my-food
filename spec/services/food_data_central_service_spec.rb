572c9645f46df52ee221b3fbc52e429e2f5c5c93
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-30 10:47:38 -0600

    Add more to the service test to make sure any food search works

--- spec/services/food_data_central_service_spec.rb
+++ spec/services/food_data_central_service_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 describe "Food Data Central Service" do
-  it "can get list of foods from API" do
+  it "can connect to API and get list of foods with a certain ingredient" do
     food_service  = FoodDataCentralService.new
 
-    expect(food_service.get_foods("sweet potatoes").class).to eq(Hash)
-    expect(food_service.get_foods("sweet potatoes")[:foods].class).to eq(Array)
+    food = "sweet potatoes"
+    expect(food_service.get_foods(food).class).to eq(Hash)
+    expect(food_service.get_foods(food)[:foods].class).to eq(Array)
+
+    food = "apples"
+    expect(food_service.get_foods(food).class).to eq(Hash)
+    expect(food_service.get_foods(food)[:foods].class).to eq(Array)
   end
 end
